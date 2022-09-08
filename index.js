72098db75e05cfdd4aff363700e6fae1486cd766
unknown <you@example.com>
2022-09-08 13:07:57 +0545

    index out

--- index.js
+++ index.js
@@ -32,6 +32,16 @@ app.use("/api/user", require("./routes/app.user"));
 app.use(errors.errorHandler);
 // app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
+if (process.env.NODE_ENV == "production") {
+  // app.use(express.static("client/build"));
+  // const path = require("path");
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
+  app.get("*", (req, res) => {
+    return "Successfullu Deployed";
+  });
+}
 app.listen(process.env.port || 4000, function () {
   console.log("Ready to Go!");
 });
