e8d90a23c469102a613506fdff36e41d4808b496
unknown <you@example.com>
2022-09-08 13:58:38 +0545

    index.js

--- index.js
+++ index.js
@@ -8,28 +8,28 @@ const errors = require("./middleware/errors");
 // const swaggerUI = require("swagger-ui-express"),
 //   swaggerDocument = require("./swagger.json");
 
-mongoose.Promise = global.Promise;
-mongoose
-  .connect(MONGO_DB_CONFIG.DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(
-    () => {
-      console.log("Database Connected" + " " + 4000);
-    },
-    (error) => {
-      console.log("Database cant be connected" + error);
-    }
-  );
-
-app.use(express.json());
-app.get("/", function (req, res) {
-  res.send("Successfully Deployed");
+// mongoose.Promise = global.Promise;
+// mongoose
+//   .connect(MONGO_DB_CONFIG.DB, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(
+//     () => {
+//       console.log("Database Connected" + " " + 4000);
+//     },
+//     (error) => {
+//       console.log("Database cant be connected" + error);
+//     }
+//   );
+app.get("/", (req, res) => {
+  res.status(200).send("Hello server is running").end();
 });
-app.use("/uploads", express.static("uploads"));
-app.use("/api", require("./routes/app.routes"));
-app.use("/api/user", require("./routes/app.user"));
+
+// app.use(express.json());
+// app.use("/uploads", express.static("uploads"));
+// app.use("/api", require("./routes/app.routes"));
+// app.use("/api/user", require("./routes/app.user"));
 // app.use(errors.errorHandler);
 // app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
